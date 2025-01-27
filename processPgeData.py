62b682bdf465a48017dcc323aa08d8ef9fabc9f2
Bruce Hill <bruce@kynehill.com>
2025-01-27 03:46:32 -0800

    Got 1/1/2024 looking good for each rate type

--- processPgeData.py
+++ processPgeData.py
@@ -111,26 +111,99 @@ class   HourlyProj:
         self.Grid      = 0      # kWh
         self.Battery   = 0      # kWh
         self.Charging  = 0      # kWh
-        self.Solar     = 0      # kWh
+        self.NewSolar  = 0      # kWh
+        self.OldSolar  = 0      # kWh
         self.Export    = 0      # kWh
         self.TimeOfDay = datetime.now()
-    def __init__( self, grid, battery, charge, solar, export, time ):
+    def __init__( self, time, grid=0, battery=0, charging=0, newSolar=0, oldSolar=0, export=0 ):
         self.Grid      = grid      # kWh
         self.Battery   = battery   # kWh
-        self.Charging  = charge    # kWh
-        self.Solar     = solar     # kWh
+        self.Charging  = charging  # kWh
+        self.NewSolar  = newSolar  # kWh
+        self.OldSolar  = oldSolar  # kWh
         self.Export    = export    # kWh
         self.TimeOfDay = time
+    def __str__( self ):
+        #return f"{self.TimeOfDay}: Grid={self.Grid:>6.2f}, Charging={self.Charging:>6.2f}, Battery={self.Battery:>6.2f}, Export={self.Export:>6.2f}" 
+        return f"{self.TimeOfDay}: Grid={self.Grid:>6.2f}, Charging={self.Charging:>6.2f}, Battery={self.Battery:>6.2f}, Export={self.Export:>6.2f}, NewSolar={self.NewSolar:>6.2f}, OldSolar={self.OldSolar:>6.2f}" 
 
+    def ApplyBatteryToGrid( self ):
+        if self.Grid > 0 and self.Battery > 0:
+            if self.Battery >= self.Grid:
+                self.Battery -= self.Grid
+                self.Grid = 0
+            else:
+                self.Grid -= self.Battery
+                self.Battery = 0
+        if self.Grid < 0:
+            print( "Error: {self}" )
+ 
+    def ApplyNewSolarToBattery( self, options ):
+        if self.NewSolar <= 0:
+            return
+        if self.Battery >= options.MaxBattery:
+            return
+        availCharge = self.NewSolar * options.Efficiency / 100
+        newCharge = min( availCharge, options.MaxBattery - self.Battery )
+        self.Battery = min( newCharge, options.MaxBattery )
+        self.Charging += newCharge
+        self.NewSolar = max( 0, self.NewSolar - newCharge / (options.Efficiency/100) )
+ 
+    def ApplyOldSolarToBattery( self, options ):
+        if self.OldSolar <= 0:
+            return
+        if self.Battery >= options.MaxBattery:
+            return
+        availCharge = self.OldSolar * options.Efficiency / 100
+        newCharge = min( availCharge, options.MaxBattery - self.Battery )
+        self.Battery = min( newCharge, options.MaxBattery )
+        self.Charging += newCharge
+        self.OldSolar = max( 0, self.OldSolar - newCharge / (options.Efficiency/100) )
+
+    def ApplyOldSolarToGrid( self ):
+        if self.Grid > 0 and self.OldSolar > 0:
+            if self.OldSolar >= self.Grid:
+                self.OldSolar -= self.Grid
+                self.Grid = 0
+            else:
+                self.Grid -= self.OldSolar
+                self.OldSolar = 0
+        if self.Grid < 0:
+            print( "Error: {self}" )
+ 
+    def ApplyNewSolarToGrid( self ):
+        if self.Grid > 0 and self.NewSolar > 0:
+            if self.NewSolar >= self.Grid:
+                self.NewSolar -= self.Grid
+                self.Grid = 0
+            else:
+                self.Grid -= self.NewSolar
+                self.NewSolar = 0
+        if self.Grid < 0:
+            print( "Error: {self}" )
+ 
+    def ExportNewSolarToGrid( self ):
+        if self.NewSolar > 0:
+            self.Export += self.NewSolar
+            self.NewSolar = 0
+
+    def ExportOldSolarToGrid( self ):
+        if self.OldSolar > 0:
+            self.Export += self.OldSolar
+            self.OldSolar = 0
+ 
 #   Options for home solar projections
 #   opt1 = Option( 'E-ELEC', '1.0', 13.5, 5600 )
 class   Option:
-    def __init__( self, ratePlan, nem, maxBattery, newSolar ):
-        self.RatePlan   = ratePlan    # 'E-ELEC' or 'E-TOU-C' or 'E-TOU-D'
-        self.NEM        = nem         # '1.0' or '3.0'
-        self.MaxBattery = maxBattery  # kWh
-        self.NewSolar   = newSolar    # Yearly kWh
-        self.Proj       = []
+    def __init__( self, ratePlan, nem, maxBattery, newSolarProd, efficiency=95 ):
+        self.RatePlan     = ratePlan      # 'E-ELEC' or 'E-TOU-C' or 'E-TOU-D'
+        self.NEM          = nem           # '1.0' or '3.0'
+        self.MaxBattery   = maxBattery    # kWh
+        self.Efficiency   = efficiency    # %
+        self.NewSolarProd = newSolarProd  # Yearly kWh
+        self.Proj         = []
+    def __str__( self ):
+        return f"RatePlan={self.RatePlan:>7}, NEM={self.NEM}, MaxBattery={self.MaxBattery}, Efficiency={self.Efficiency}, NewSolarProd={self.NewSolarProd}" 
 
 def DebugPeakVsOffPeakTimes( timeOfDay ):
     debugWinter2Summer = datetime.datetime( year=timeOfDay.year, month=6, day = 1 )
@@ -143,6 +216,11 @@ def DebugPeakVsOffPeakTimes( timeOfDay ):
             return True
     return False
 
+def DebugThisDay( timeOfDay, year, month, day ):
+    if timeOfDay.year == year and timeOfDay.month == month and timeOfDay.day == day:
+        return True
+    return False
+
 class   HomeSolar:
     def __init__( self ):
         self.hourlyData = []
@@ -150,32 +228,78 @@ class   HomeSolar:
 
     def AddOption( self, option ):
         self.options.append( option )
+        print( f'Adding option: {option}' )
         battery = option.MaxBattery / 2
-        option.Proj.append( HourlyProj( 0, battery, 0, 0, 0, self.hourlyData[0].TimeOfDay ) )
+        option.Proj.append( HourlyProj( time=self.hourlyData[0].TimeOfDay, battery=battery ) )
         for data in self.hourlyData:
-            usage = data.Usage
-            oldSolar = data.SolarProd
-            newSolar = oldSolar * (option.NewSolar / oldSolarYearlyProd)
+            oldSolar = -data.SolarProd
+            if oldSolar < 0.02: oldSolar = 0 # Clean up output by eliminating trivial solar kWh due to CT accuracy limits
+            newSolar = oldSolar * (option.NewSolarProd / oldSolarYearlyProd)
             verbose = False # DebugPeakVsOffPeakTimes(data.TimeOfDay)
+            verbose = DebugThisDay( data.TimeOfDay, data.TimeOfDay.year, 1, 1 )
 
+            newHour = HourlyProj( time=data.TimeOfDay, grid=data.Usage, battery=battery, oldSolar=oldSolar, newSolar=newSolar )
+            #if verbose: print( newHour )
             if isPeakTime( data.TimeOfDay, option.RatePlan ):
+                #
                 # Handle Peak periods
-                if verbose and isSummerPeakTime( data.TimeOfDay, option.RatePlan ):
-                    print( "%s: %s is Summer Peak" % ( option.RatePlan, data.TimeOfDay ) )
-                if verbose and isWinterPeakTime( data.TimeOfDay, option.RatePlan ):
-                    print( "%s: %s is Winter Peak" % ( option.RatePlan, data.TimeOfDay ) )
+                #
+                # Use NewSolar first
+                newHour.ApplyNewSolarToGrid( )
+                if option.NEM == '1.0':
+                    # Battery then OldSolar
+                    newHour.ApplyBatteryToGrid( )
+                    newHour.ApplyOldSolarToGrid( )
+                else:
+                    # OldSolar then Battery
+                    newHour.ApplyBatteryToGrid( )
+                    newHour.ApplyOldSolarToGrid( )
             elif isPartialPeakTime( data.TimeOfDay, option.RatePlan ):
-                if verbose and isSummerPartialPeakTime( data.TimeOfDay, option.RatePlan ):
-                    print( "%s: %s is Summer PartialPeak" % ( option.RatePlan, data.TimeOfDay ) )
-                if verbose and isWinterPartialPeakTime( data.TimeOfDay, option.RatePlan ):
-                    print( "%s: %s is Winter PartialPeak" % ( option.RatePlan, data.TimeOfDay ) )
+                #
                 # Handle PartialPeak periods
+                #
+                # Rough estimate of upcoming 5 hours of peak usage
+                estPeakUsage = data.Usage * 5.5
+                newHour.ApplyNewSolarToGrid( )
+                if option.NEM == '1.0':
+                    # Only use battery if we can cover peak usage too
+                    if newHour.Battery > data.Usage + estPeakUsage:
+                        newHour.ApplyBatteryToGrid( )
+                    newHour.ApplyOldSolarToGrid( )
+                else:
+                    newHour.ApplyOldSolarToGrid( )
+                    # Only use battery if we can cover peak usage too
+                    if newHour.Battery > data.Usage + estPeakUsage:
+                        newHour.ApplyBatteryToGrid( )
             elif isOffPeakTime( data.TimeOfDay, option.RatePlan ):
-                if verbose and isSummerOffPeakTime( data.TimeOfDay, option.RatePlan ):
-                    print( "%s: %s is Summer OffPeak" % ( option.RatePlan, data.TimeOfDay ) )
-                if verbose and isWinterOffPeakTime( data.TimeOfDay, option.RatePlan ):
-                    print( "%s: %s is Winter OffPeak" % ( option.RatePlan, data.TimeOfDay ) )
+                #
                 # Handle OffPeak periods
+                #
+                newHour.ApplyNewSolarToGrid( )
+                newHour.ApplyOldSolarToGrid( )
+                
+                newHour.ApplyNewSolarToBattery( option )
+                if option.NewSolarProd == 0 or option.NEM == '3.0':
+                    # If we add new solar under NEM 1.0 via non-export, 
+                    # we can only charge the battery from NewSolar
+                    newHour.ApplyOldSolarToBattery( option )
+
+                # Apply remaining battery to grid
+                newHour.ApplyBatteryToGrid( )
+
+            # Apply remaining peak or partial peak NewSolar to battery
+            newHour.ApplyNewSolarToBattery( option )
+
+            # Export excess solar to grid
+            if option.NEM == '3.0':
+                newHour.ExportNewSolarToGrid( )
+            newHour.ExportOldSolarToGrid( )
+            newHour.TimeOfDay += timedelta( minutes=59, seconds=59 )
+
+            # Hold remaining battery for next hour
+            battery = newHour.Battery
+            if verbose:
+                print( newHour )
 
     def ProcessDataFiles( self, pgeData, vueData, verbose = False ):
         # Note: Both data files have 23 entries for start of DST, 3/10/24
@@ -191,7 +315,7 @@ class   HomeSolar:
                 usage = pgeData[hourTime]
             # solarProd is negative and needs to be added
             # to PGE usage to reflect actual usage w/o solar
-            usage = usage + solarProd
+            usage = max( 0, usage + solarProd )
             if hourTime < extraHour:
                 self.hourlyData.append( HourlyData( usage, solarProd, hourTime ) )
         if verbose:
@@ -227,7 +351,7 @@ def readPgeData( pgeDataFile, verbose=False ):
             code = 0
             message = v
         print(repr(sys.exception()))
-        sys.stderr.write('I/O Error %s: Could not open "%s": %s\n' % (pgeDataFile, str(message)))
+        sys.stderr.write('I/O Error %s: Could not open "%s"\n' % (pgeDataFile, str(message)))
         raise
     except:
         print(repr(sys.exception()))
@@ -337,13 +461,13 @@ def main(argv=None):
     myHomeSolar = HomeSolar()
     myHomeSolar.ProcessDataFiles( pgeData, vueData, options.verbose )
 
-    myHomeSolar.AddOption( Option( 'E-TOU-D', '1.0', 0, 0 ) )
-    myHomeSolar.AddOption( Option( 'E-TOU-C', '1.0', 0, 0 ) )
+    #myHomeSolar.AddOption( Option( 'E-TOU-D', '1.0', 0, 0 ) )
+    #myHomeSolar.AddOption( Option( 'E-TOU-C', '1.0', 0, 0 ) )
     myHomeSolar.AddOption( Option( 'E-ELEC', '1.0', 13.5, 0 ) )
-    # myHomeSolar.AddOption( Option( 'E-ELEC', '1.0', 13.5, 5600 ) )
-    # myHomeSolar.AddOption( Option( 'E-ELEC', '1.0', 13.5, 11214 ) )
-    # myHomeSolar.AddOption( Option( 'E-ELEC', '3.0', 13.5, 5600 ) )
-    return None
+    #myHomeSolar.AddOption( Option( 'E-ELEC', '1.0', 13.5, 5600 ) )
+    #myHomeSolar.AddOption( Option( 'E-ELEC', '1.0', 13.5, 11214 ) )
+    #myHomeSolar.AddOption( Option( 'E-ELEC', '3.0', 13.5, 5600 ) )
+    return 0
 
 if __name__ == '__main__':
     status = main()
