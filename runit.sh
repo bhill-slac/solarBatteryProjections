757d2b0dcb3c24289fa29e4caf8582b4bd322fea
Bruce Hill <bruce@kynehill.com>
2025-01-26 20:36:29 -0800

    Fixed missing quotes

---
+++ runit.sh
@@ -0,0 +1,2 @@
+#! bash
+python -m pdb processPgeData.py  -f '..\pge-1f41d63d-d22b-11e3-9228-22ec3b633f13-DailyUsageData (2)\pge_electric_usage_interval_data_Service 2_2_2024-01-01_to_2024-04-14.csv' -f '..\pge-1f41d63d-d22b-11e3-9228-22ec3b633f13-DailyUsageData (2)\pge_electric_usage_interval_data_Service 2_2_2024-04-15_to_2024-12-31.csv' -v -e '..\data-export-40973--4363628304760783698\B77670-Main_panel-1H.csv'
